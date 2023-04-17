523c5546af0efd113e5b42ab2702cde95f6df58f
alexzarembovskiy <alexzarembovskiy@gmail.com>
2023-04-17 21:48:28 +0200

    historical records added

--- task1/weather_dag/dags/weather-dag.py
+++ task1/weather_dag/dags/weather-dag.py
@@ -1,14 +1,16 @@
 import requests
+import time
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 
-def _extract_data():
-    url = "https://api.openweathermap.org/data/3.0/onecall"
+def _extract_data(execution_date):
+    url = "https://api.openweathermap.org/data/3.0/onecall/timemachine"
     params = {'lat': 49.841952, 
               'lon': 24.0315921, 
+              'dt': int(time.mktime(execution_date.timetuple())),
               'exclude': "minutely,hourly,daily,alerts",
               'units': "metric",
               "APPID": Variable.get("weather_appid")
@@ -17,17 +19,18 @@ def _extract_data():
     return response.json()
 
 def _process_weather(ti):
-    info = ti.xcom_pull(task_ids="extract_data", key='return_value')
-    timestamp = info["current"]["dt"]
-    temp = info["current"]["temp"]
-    humidity = info["current"]["humidity"]
-    clouds = info["current"]["clouds"]
-    wind_speed = info["current"]["wind_speed"]
+    info = ti.xcom_pull(task_ids="extract_data", key='return_value')["data"][0]
+    timestamp = info["dt"]
+    temp = info["temp"]
+    humidity = info["humidity"]
+    clouds = info["clouds"]
+    wind_speed = info["wind_speed"]
     return timestamp, temp, humidity, clouds, wind_speed
 
 with DAG(dag_id="weather", 
          schedule_interval="@daily", 
-         start_date=days_ago(2)
+         start_date=days_ago(5),
+         catchup=True
          ) as dag:
     
     create_postgres_table = PostgresOperator(
